d5874eee85ef1a883f93e248fe22d44b3786b783
Charles Arciniega <charlesarcin@gmail.com>
2014-10-08 20:51:13 -0700

    Add listwise integration
    Last minute change. Forgot to put the key in an ENV variable.

--- app/controllers/concerns/list_wise.rb
+++ app/controllers/concerns/list_wise.rb
@@ -5,7 +5,7 @@ module ListWise
   @base = 'https://api.listwisehq.com/clean/quick.php'
 
   def self.email_status(email)
-    options = { query: { email: email, api_key: '3775e817b40483db9ec9f48119cc96dc' }}
+    options = { query: { email: email, api_key: ENV['LISTWISE_API_KEY'] }}
     response = HTTParty.get(@base, options)
     response.parsed_response["email_status"]
   end
