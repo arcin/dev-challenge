04bd56e55d511df6db894d933a1ea50b2d428112
Charles Arciniega <charlesarcin@gmail.com>
2014-10-08 20:13:01 -0700

    Add google maps integration
    Display color codedmarkers on a google map. This challenge required some request
    throttling because of the 10 query bottleneck that google sets. I set an interval
    to make requests in batches, one request per address type.
    
    I chose to go the OO javascript route to keep code from getting too messy and
    difficult to debug. It's a little long, but each function has a single
    responsibility and the Gmap object only concerns itself with manipulating the
    map.
    
    app/assets/javascripts/maps_javascript.js
      - create a Gmap class that manages the state of the google map
        and concerns itself the manipulation of markers.
      - create GMapsHelper module to initialize a google map and encapsulate
        some start up methods.
      - Most importantly, use setInterval to deal with the rate limiting set
        by google.
    
    app/assets/stylesheets/maps.css.scss
      - add some styles to the map canvas so that it shows up on the page.
    
    app/views/layouts/application.html.erb
      - add script for googlemaps api
    
    app/views/maps/show.html.haml
      - move some divs around for better map placement

--- app/assets/javascripts/maps_javascript.js
+++ app/assets/javascripts/maps_javascript.js
@@ -1,5 +1,6 @@
 // You can use CoffeScript in maps.js.coffee if you'd rather.
 
+//### Add google maps integration
 $(document).on('page:load', function (event) {
   var plot_me = {
     visited: [
@@ -23,4 +24,101 @@ $(document).on('page:load', function (event) {
       '1141 N Brand Blvd #306, Glendale, CA 91202'
     ]
   }
+
+  var map = GMapsHelper.initialize(plot_me);
+  // 1510 was the fastest I could ping the Google api's without getting
+  // rate limited.
+  requestInterval = setInterval(function(){ map.spawnMarkers() }, 1510);
 });
+
+
+var domDependencies = {
+  canvasID: 'map-canvas'
+};
+
+var GMapsHelper = (function(d){
+  // Private module stuff
+  var icons = {
+     visited: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+     revisit: 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png',
+     unvisited: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+   };
+  var _createMap = function(){
+    var mapOptions, map;
+    mapOptions = {
+      center: {lat: 34.133764, lng: -118.1507633},
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById(d["canvasID"]),
+        mapOptions);
+    return map
+  }
+  var _createGeocoder = function(){
+    return new google.maps.Geocoder();
+  }
+  var _addressesToMarkers = function(addressObj){
+    var groups = [];
+    for (addressType in addressObj) {
+      var group = {};
+      group.addresses = addressObj[addressType];
+      group.type = addressType;
+      groups.push(group);
+    }
+    return groups;
+  }
+  var _initializeMapObject = function(addresses){
+    var initOptions = {
+      map: _createMap(),
+      geocoder: _createGeocoder(),
+      icons: icons,
+      markerGroups: _addressesToMarkers(addresses)
+    }
+    return new GMap(initOptions);
+  }
+  // GMap class
+  var GMap = function(opts) {
+    this.map = opts["map"];
+    this.geocoder = opts["geocoder"];
+    this.icons = opts["icons"];
+    this.markerGroups = opts["markerGroups"]
+  }
+
+  GMap.prototype.placeMarker = function(address, type){
+    var that, marker;
+    that = this;
+    that.geocoder.geocode({address: address}, function(results, status){
+      if (status == google.maps.GeocoderStatus.OK) {
+          marker = new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: that.map,
+            icon: that.icons[type]
+          });
+      } else {
+        console.log("awwww snap ", status);
+      }
+    })
+  }
+
+  GMap.prototype.placeAllMarkers = function(addresses, type) {
+    for(var i = 0; i < addresses.length; i++) {
+      this.placeMarker(addresses[i], type)
+    }
+  }
+  GMap.prototype.spawnMarkers = function(){
+    var group;
+    if (this.markerGroups.length === 0) {
+      clearInterval(requestInterval);
+      return
+    }
+    group = this.markerGroups.pop();
+    this.placeAllMarkers(group.addresses, group.type)
+  }
+
+  // Public module stuff
+  return {
+    initialize: function(addresses){
+      return _initializeMapObject(addresses)
+    }
+  }
+})(domDependencies)
