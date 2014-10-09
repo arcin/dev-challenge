6affa6b100afaf730c82b67a3113e88c2eb6019c
Charles Arciniega <charlesarcin@gmail.com>
2014-10-08 20:39:44 -0700

    Testing: Docs Controller
    super simple tests for a super simple controller.

--- test/controllers/docs_controller_test.rb
+++ test/controllers/docs_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DocsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  ### Testing: Docs Controller
+  test "should get readme" do
+    get :readme
+    assert_response :success
+  end
+
+  test "readme should render the correct template" do
+    get :readme
+    assert_template :readme
+  end
 end
