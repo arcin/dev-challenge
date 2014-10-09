627d9950464269a5177f3bce30e1cb1e0159ed3a
Charles Arciniega <charlesarcin@gmail.com>
2014-10-08 20:49:08 -0700

    Code Review: Docs Controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -6,5 +6,10 @@ class DocsController < ApplicationController
 
   # displays 'tasklist.md', 'comments.md', and 'section_E.md' to the viewer.
   def readme
+  ### REVIEW: The controller and action seem legit, but I was thrown off by the
+  ### content being rendered. 'Docs' and 'Readme' make me think of documentation,
+  ### but the stuff being rendered is pretty much just miscellaneous info.
+  ### Conceptually, It might work better to fit them into a separate controller
+  ### and action. You know, for science.
   end
 end
